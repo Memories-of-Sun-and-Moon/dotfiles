d8e8a43965527f0b7cb4e3334a4a7ef5642c9dbe
ouchiPC <univ.t1c2ha3@gmail.com>
2024-12-19 17:12:24 +0900

    add lualine.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,11 +21,60 @@ require("lazy").setup({
 		config = function()
 			vim.cmd[[colorscheme tokyonight]]
 		end
+	},
+	"HiPhish/rainbow-delimiters.nvim",
+	"nvim-treesitter/playground",
+	{
+	    'nvim-lualine/lualine.nvim',
+	    dependencies = { 'nvim-tree/nvim-web-devicons' }
 	}
 })
   
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+
 require'nvim-treesitter.configs'.setup {
-	ensure_installed = {"cpp", "lua", "markdown" },
+	ensure_installed = {"cpp", "lua", "markdown", "python" },
 	auto_install = true,
 }
 
+vim.opt.clipboard:append({"unnamedplus"})
