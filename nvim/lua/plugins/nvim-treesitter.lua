1ef911c09ded6a330165f4e4180c7bdc6b3c7899
mmrz(from ubuntu24) <univ.t1c2ha3@gmail.com>
2025-11-07 21:07:01 +0900

    render-markdown.lua の追加

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,12 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		ensure_installed = {"cpp", "lua", "markdown", "python" },
+		build = ":TSUpdate", 
+		ensure_installed = {"cpp", "lua", "markdown", "python", "markdown", "markdown_inline" },
 		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
 	}
 }
